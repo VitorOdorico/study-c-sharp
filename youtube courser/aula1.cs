31966a90e7fa0f1864d906c4489755e6f99c988a
VitorOdorico <ve743137@gmail.com>
2022-10-21 12:56:12 -0300

    fist call one, couser youtube

---
+++ youtube courser/aula1.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1{
+    class Program{
+        static void Main(string[] args){
+            // Tipo, identificador .
+            char primeiraLetraNome;
+            string nome;
+            Console.WriteLine(primeiraLetraNome);
+            
+            float valor;
+            double valorAlto;
+            bool primeiraLetraComecaM;
+            primeiraLetraComecaM = 'V';
+            valorAlto = 3.145367;
+            valor = 3.14;
+        }
+    }
+}
