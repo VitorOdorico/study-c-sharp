9ccc18297f1b9cbe9192de7fcf6ded1935dfa263
VitorOdorico <ve743137@gmail.com>
2022-10-29 17:56:29 -0300

    minha primeira aplicação em C#

---
+++ youtube courser/primeiros projetos/app.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app{
+    class Program{
+        static void Main(){
+            Console.writeLine("Olá mundo");// imprimir informação na tela
+
+        }
+    }
+}
